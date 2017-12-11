463ea97d55e74f8a4ab73e1da1f75ee67f269d52
Kunuk Nykjær <kunukn@gmail.com>
2017-12-11 22:56:11 +0100

    updates

--- index.js
+++ index.js
@@ -1,85 +1,85 @@
-import backInOut from './back-in-out'
-import backIn from './back-in'
-import backOut from './back-out'
+import backInOut from './back-in-out';
+import backIn from './back-in';
+import backOut from './back-out';
 
-import bounceInOut from './bounce-in-out'
-import bounceIn from './bounce-in'
-import bounceOut from './bounce-out'
+import bounceInOut from './bounce-in-out';
+import bounceIn from './bounce-in';
+import bounceOut from './bounce-out';
 
-import circInOut from './circ-in-out'
-import circIn from './circ-in'
-import circOut from './circ-out'
+import circInOut from './circ-in-out';
+import circIn from './circ-in';
+import circOut from './circ-out';
 
-import cubicInOut from './cubic-in-out'
-import cubicIn from'./cubic-in'
-import cubicOut from './cubic-out'
+import cubicInOut from './cubic-in-out';
+import cubicIn from './cubic-in';
+import cubicOut from './cubic-out';
 
-import elasticInOut from './elastic-in-out'
-import  elasticIn from './elastic-in'
-import  elasticOut from './elastic-out'
+import elasticInOut from './elastic-in-out';
+import elasticIn from './elastic-in';
+import elasticOut from './elastic-out';
 
-import expoInOut from './expo-in-out'
-import expoIn from './expo-in'
-import expoOut from './expo-out'
+import expoInOut from './expo-in-out';
+import expoIn from './expo-in';
+import expoOut from './expo-out';
 
-import linear from './linear'
+import linear from './linear';
 
-import quadInOut from './quad-in-out'
-import quadIn from './quad-in'
-import quadOut from './quad-out'
+import quadInOut from './quad-in-out';
+import quadIn from './quad-in';
+import quadOut from './quad-out';
 
-import quartInOut from './quart-in-out'
-import quartIn from './quart-in'
-import quartOut from './quart-out'
+import quartInOut from './quart-in-out';
+import quartIn from './quart-in';
+import quartOut from './quart-out';
 
-import quintInOut from './quint-in-out'
-import quintIn from './quint-in'
-import quintOut from './quint-out'
+import quintInOut from './quint-in-out';
+import quintIn from './quint-in';
+import quintOut from './quint-out';
 
-import sineInOut from './sine-in-out'
-import sineIn from './sine-in'
-import sineOut from './sine-out'
+import sineInOut from './sine-in-out';
+import sineIn from './sine-in';
+import sineOut from './sine-out';
 
 export default {
-	backInOut,
-	backIn,
-	backIn,
+  backInOut,
+  backIn,
+  backIn,
 
-	bounceInOut,
-	bounceIn,
-	bounceOut,
+  bounceInOut,
+  bounceIn,
+  bounceOut,
 
-	circInOut,
-	circIn,
-	circOut,
-	
-	cubicInOut,
-	cubicIn,
-	cubicOut,
+  circInOut,
+  circIn,
+  circOut,
 
-	elasticInOut,
-	elasticIn,
-	elasticOut,
+  cubicInOut,
+  cubicIn,
+  cubicOut,
 
-	expoInOut,
-	expoIn,
-	expoOut,
+  elasticInOut,
+  elasticIn,
+  elasticOut,
 
-	linear, 
+  expoInOut,
+  expoIn,
+  expoOut,
 
-	quadInOut,
-	quadIn,
-	quadOut,
+  linear,
 
-	quartInOut,
-	quartIn,
-	quartOut,
+  quadInOut,
+  quadIn,
+  quadOut,
 
-	quintInOut,
-	quintIn,
-	quintOut,
+  quartInOut,
+  quartIn,
+  quartOut,
 
-	sineInOut,
-	sineIn,
-	sineOut,
-}
+  quintInOut,
+  quintIn,
+  quintOut,
+
+  sineInOut,
+  sineIn,
+  sineOut,
+};
